a82b0ae24c53f15991e21cbe1a935ba103e78747
dalil16 <hermez.dalil@gmail.com>
2024-07-25 15:38:48 +0100

    ajout d'un hebergement du serveur de commentaire JSON

---
+++ avis.js
@@ -0,0 +1,29 @@
+
+export function ajoutListenersAvis() {
+    const piecesElements = document.querySelectorAll(".fiches article button");
+
+    for (let i = 0; i < piecesElements.length; i++) {
+      piecesElements[i].addEventListener("click", async function (event) {
+        const id = event.target.dataset.id;
+        const reponse= await fetch(`http://localhost:8081/pieces/${id}/avis`);
+        const avis= (await reponse.json() );
+
+        const pieceElement = event.target.parentElement;
+
+        const avisElement = document.createElement("p");
+        for (let i = 0; i < avis.length; i++) {
+        avisElement.innerHTML += `${avis[i].utilisateur}: ${avis[i].commentaire} <br>`;
+        pieceElement.appendChild(avisElement);
+        }
+
+        /*
+        for (let i = 0; i < avis.length; i++) {
+            console.log(`${avis[i].utilisateur}: ${avis[i].commentaire}`);
+            
+            //console.log(
+           // alert(`${avis[i].utilisateur}: ${avis[i].commentaire} `)
+         }
+           /* ... */
+      });
+    }
+}
